8726fca4e063a2640f847177a7fddc7e61c2d220
Xuewen Wang <xwwang@uga.edu>
2024-07-16 12:29:55 -0500

    Update find_gene_hmm.sh

--- find_gene_hmm.sh
+++ find_gene_hmm.sh
@@ -5,7 +5,7 @@ inFastaSeq="STR_good.fasta"
 
 clustalw -INFILE=$inFastaSeq -OUTFILE=$inFastaSeq.aln
 
-input=$inFastaSeq.aln #bait gene  aligment from previous pep.fasta
+input=$inFastaSeq.aln #bait gene  alignment from previous pep.fasta
 
 
 ## Build the HMM Profile: hmmer version HMMER 3.4 (Aug 2023); http://hmmer.org/
@@ -14,4 +14,4 @@ hmmbuild profile.hmm $input
 hmmsearch -E 1e-5 --tblout ${GeneName}_all_genes_in_genome.n.txt profile.hmm $genome
 
 #filter on evalue and hit score
-python $scriptdir/get_covered_hit_fromHmm.py -i ${GeneName}_all_genes_in_genome.n.txt -o ${GeneName}_all_genes_in_genome.hmm.filtered.tsv -e 1e-10 -s 100
+python get_covered_hit_fromHmm.py -i ${GeneName}_all_genes_in_genome.n.txt -o ${GeneName}_all_genes_in_genome.hmm.filtered.tsv -e 1e-10 -s 100
